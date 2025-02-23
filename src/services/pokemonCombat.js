3d72c961c573d7e1e5407ade66dded15c6a83bda
Julien Marchand <nifrael66@gmail.com>
2025-02-23 17:29:18 +0100

    Début système combat

--- src/services/pokemonCombat.js
+++ src/services/pokemonCombat.js
@@ -1,1 +1,12 @@
 import { getPokemon } from "./pokemonFetch";
+
+function setStatistic(pokemon) {
+  const health = pokemon.stats.hp;
+  const defense = pokemon.stats.def;
+  const attack = pokemon.stats.attack;
+}
+
+function pokemonAttack(attacker, defender) {
+  const damage = attacker.stats.attack * Math.random();
+  const newHealth = defender.stats.hp - damage;
+}
