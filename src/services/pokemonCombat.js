7b16d2a7142529a9d99cdd3a2f58c824e805cbc7
Julien Marchand <nifrael66@gmail.com>
2025-03-01 12:32:58 +0100

    tests combats

--- src/services/pokemonCombat.js
+++ src/services/pokemonCombat.js
@@ -4,14 +4,20 @@ function calculDamage(attaquant) {
   let multiplicateur = Math.random();
   let damage = attaquant.attack * multiplicateur;
   damage = Math.trunc(damage);
-  return damage;
+  return `${attaquant.name} inflige ${damage} points de dégats.`;
 }
 
 function calculDefense(defenseur) {
-  let multiplicateur = Math.random();
-  let bonus = defenseur.defense * multiplicateur;
+  let bonus = defenseur.defense / 5;
   bonus = Math.trunc(bonus);
-  return bonus;
+  return `${defenseur.name} résiste de ${bonus} points de défense.`;
+}
+
+export function useAttack(attaquant, defenseur) {
+  const degats = calculDamage(attaquant);
+  const health = calculDefense(defenseur);
+  defenseur.hp = degats + health;
+  return `Il reste ${defenseur.hp} points de vie à ${defenseur.name}.`;
 }
 
 class Pokemon {
@@ -21,15 +27,9 @@ class Pokemon {
     this.attack = data[0].stats.attack;
     this.defense = data[0].stats.defense;
     this.speed = data[0].stats.speed;
+    this.image = data[0].image;
     this.primaryType = data[0].types[0].name;
     this.secondaryType = data[0].types[1] ? data[0].types[1].name : null;
-  }
-
-  useAttack(defenseur) {
-    const degats = calculDamage(this);
-    const health = calculDefense(defenseur);
-    defenseur.hp = degats + health;
-    return `Il reste ${defenseur.hp} points de vie à ${defenseur.name}.`;
   }
 }
 
